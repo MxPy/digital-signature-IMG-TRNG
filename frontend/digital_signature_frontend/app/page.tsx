0be28f2074aa15ca864b2c6d440ba6832a7c5d14
Maximilian Patalas <maksymilianpatalas@gmail.com>
2024-05-31 18:22:34 +0200

    Final

--- frontend/digital_signature_frontend/app/page.tsx
+++ frontend/digital_signature_frontend/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import "./page.css"
 
 export default function Home() {
   return (
@@ -8,8 +7,8 @@ export default function Home() {
       <div className="hero min-h-screen" style={{backgroundImage: 'url(https://www.purepc.pl/image/news/2023/03/09_discord_testuje_mozliwosci_sztucznej_inteligencji_ai_odpowie_na_pytania_uzytkownikow_i_pomoze_w_moderacji_serwera_1_b.png)'}}>
         <div className="hero-overlay bg-opacity-70"></div>
         <div className="hero-content text-center">
-          <div className="max-w-2xl-test">
-                <Link href="/sign" className="btn btn-wide btn-primary">Sign Your File</Link>
+          <div className="max-w-2xl">
+                <Link href="/sign" className="btn btn-wide btn-primary m-2">Sign Your File</Link>
                 <Link href="/check" className="btn btn-wide btn-primary">Verify Your File</Link>
           </div>
         </div>
